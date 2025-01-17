8ab5e5de3f1d2f0c50250718bc53fcdd652d5e10
Yaroslav Moria <5eeman@users.noreply.github.com>
2025-01-17 15:13:24 +0100

    Updated tests.

--- test/identity/domain/use_cases/identity/update_identity_use_case_test.dart
+++ test/identity/domain/use_cases/identity/update_identity_use_case_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:polygonid_flutter_sdk/identity/domain/entities/identity_entity.dart';
-import 'package:polygonid_flutter_sdk/identity/domain/exceptions/identity_exceptions.dart';
 import 'package:polygonid_flutter_sdk/identity/domain/repositories/identity_repository.dart';
 import 'package:polygonid_flutter_sdk/identity/domain/use_cases/identity/create_identity_use_case.dart';
 import 'package:polygonid_flutter_sdk/identity/domain/use_cases/identity/get_identity_use_case.dart';
