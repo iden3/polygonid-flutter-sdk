0311fbf31a4f28a61a90157ac9f2df1a4114ce76
Yaroslav Moria <5eeman@users.noreply.github.com>
2025-01-20 13:42:23 +0100

    Cleanup.

--- lib/proof/domain/use_cases/get_gist_mtproof_use_case.dart
+++ lib/proof/domain/use_cases/get_gist_mtproof_use_case.dart
@@ -1,5 +1,4 @@
 import 'package:polygonid_flutter_sdk/common/domain/domain_logger.dart';
-import 'package:polygonid_flutter_sdk/common/domain/entities/chain_config_entity.dart';
 import 'package:polygonid_flutter_sdk/common/domain/use_case.dart';
 import 'package:polygonid_flutter_sdk/common/domain/use_cases/get_selected_chain_use_case.dart';
 import 'package:polygonid_flutter_sdk/common/infrastructure/stacktrace_stream_manager.dart';
